4272349873b7f0c17c91712ac4c645cf25420695
Andrey Vedischev <vedischev76@mail.ru>
2022-06-26 14:51:20 +0500

    add task 2 randomarray + bool deftri

---
+++ task2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using static System.Console;
+Clear();
+
+int size = 3;
+int[] array = GetRandomArray(size);
+WriteLine($"[{String.Join(",",array)}]");
+WriteLine($"[{String.Join(",",DefTri(array))}]");
+
+int[] GetRandomArray(int size)
+{
+    int[] arr = new int[size];
+    for(int i = 0; i < size; i++)
+    {
+        arr[i] = new Random().Next(0, 100);
+    }
+    return arr;
+}
+
+bool DefTri(int[] arr)
+{
+    int a = arr[0];
+    int b = arr[1];
+    int c = arr[2];
+    return ((a + b) > c || (a + c) > b || (b + c) > a);   
+}
