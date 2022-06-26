5927cb05f57b478c21dd7fb810bf99ac14d9105b
Andrey Vedischev <vedischev76@mail.ru>
2022-06-26 22:50:46 +0500

    add task6 copy array

---
+++ task6/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using static System.Console;
+Clear();
+
+Write("Введите элементы массива через пробел: ");
+int[] array = GetArrayFromString(ReadLine());
+WriteLine($"[{String.Join(",",array)}] -> [{String.Join(",",CopyArray(array))}]");
+
+int[] GetArrayFromString(string arrayStr)
+{
+    string[] arS = arrayStr.Split(new char[]{' ','&','!','*',','},StringSplitOptions.RemoveEmptyEntries);
+    int[] result = new int[arS.Length];
+    for(int i = 0; i < arS.Length; i++)
+    {
+        result[i] = int.Parse(arS[i]);
+
+    }
+    return result;
+
+}
+
+int[] CopyArray(int[] array)
+{
+    int[] result = new int[array.Length];
+    for(int i = 0; i < array.Length; i++)
+    {
+        result[i] = array[i];        
+    }
+    return result;
+
+}
