280280ec2656088a657ccfcf47a96b956f157335
Andrey Vedischev <vedischev76@mail.ru>
2022-06-26 22:37:33 +0500

    add task5 fibonachchi

--- task5/Program.cs
+++ task5/Program.cs
@@ -3,3 +3,17 @@ using static System.Console;
 Clear();
 
 Write("Введите число: ");
+WriteLine(string.Join(",",(NumFib(int.Parse(ReadLine())))));
+
+int[] NumFib(int num)
+{
+   int[] result = new int[num];
+   for(int i = 2; i < result.Length; i++)
+   {
+        result[0] = 0;
+        result[1] = 1;
+        result[i] = result[i - 1] + result[i - 2];
+   }
+   return result; 
+
+}
