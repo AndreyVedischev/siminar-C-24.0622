24aa91a4ae1fab219b461e3fbb0220e3069af644
Andrey Vedischev <vedischev76@mail.ru>
2022-06-26 16:15:36 +0500

    add task 3 string binnumber

---
+++ task3/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using static System.Console;
+Clear();
+
+Write("Введите число: ");
+int number10 = int.Parse(ReadLine());
+int size = $"{number10}".Length;
+WriteLine(BinNumber(number10));
+
+string BinNumber(int number)
+{
+    string bin_number = " ";
+    while(number > 0)
+    {
+        if(number%2 == 0)
+        {
+            bin_number += "1";
+        }
+        else
+        {
+            bin_number += "0";
+        }
+        number/=2;
+
+    }
+    return bin_number;
+
+}
